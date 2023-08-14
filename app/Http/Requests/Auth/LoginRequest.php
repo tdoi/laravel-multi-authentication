1605247b662be08ea0e809f45817c7a9b6c6de59
Takuo Doi <doi@fr-soft.com>
2023-08-14 18:09:16 +0900

    Apply changes for multi authentication to LoginRequest

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -41,7 +41,8 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+        $role = detect_role();
+        if (! auth($role)->attempt($this->only('email', 'password'), $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
